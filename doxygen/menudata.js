73a51807a869b41972ac2b0cb457d86d2ee19f56
Automatic Deployment (Travis CI) <espp-devel@listserv.mpip-mainz.mpg.de>
2018-05-21 07:40:33 +0000

    Documentation Update

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -74,6 +74,7 @@ var menudata={children:[
 {text:"v",url:"functions_v.html#index_v"},
 {text:"w",url:"functions_w.html#index_w"},
 {text:"x",url:"functions_x.html#index_x"},
+{text:"y",url:"functions_y.html#index_y"},
 {text:"z",url:"functions_z.html#index_z"},
 {text:"~",url:"functions_0x7e.html#index_0x7e"}]},
 {text:"Functions",url:"functions_func.html",children:[
@@ -160,8 +161,12 @@ var menudata={children:[
 {text:"o",url:"functions_eval.html#index_o"},
 {text:"p",url:"functions_eval.html#index_p"},
 {text:"r",url:"functions_eval.html#index_r"},
+{text:"s",url:"functions_eval.html#index_s"},
 {text:"t",url:"functions_eval.html#index_t"},
-{text:"w",url:"functions_eval.html#index_w"}]},
+{text:"w",url:"functions_eval.html#index_w"},
+{text:"x",url:"functions_eval.html#index_x"},
+{text:"y",url:"functions_eval.html#index_y"},
+{text:"z",url:"functions_eval.html#index_z"}]},
 {text:"Related Functions",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
